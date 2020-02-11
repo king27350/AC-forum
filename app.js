209b64105cbb4f0eab5c9b95b004332c54f45325
Chris <shian0222@gmail.com>
2020-02-12 00:09:03 +0800

    建立使用者註冊流程

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const app = express()
 const port = 3000
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`)
