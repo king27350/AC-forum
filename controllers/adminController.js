3a97e82f0c90a45351afea24055f98eca6f8304c
Chris <shian0222@gmail.com>
2020-02-14 19:23:58 +0800

    編輯一筆餐廳資料

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -35,7 +35,34 @@ const adminController = {
     return Restaurant.findByPk(req.params.id).then(restaurant => {
       return res.render('admin/restaurant', { restaurant: JSON.parse(JSON.stringify(restaurant)) })
     })
-  }
+  },
+
+  editRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id).then(restaurant => {
+      return res.render('admin/create', { restaurant: JSON.parse(JSON.stringify(restaurant)) })
+    })
+  },
+
+  putRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+
+    return Restaurant.findByPk(req.params.id).then((restaurant) => {
+      restaurant.update({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description
+      })
+        .then((restaurant) => {
+          req.flash('success_messages', "restaurant was successful update")
+          res.redirect('/admin/restaurants')
+        })
+    })
+  },
 
 }
 
