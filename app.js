4e496b5cfe3e5f74dd2d2f4c3f9a054a88f9a853
Chris <shian0222@gmail.com>
2020-02-18 16:19:21 +0800

    自訂 Handlebars Helper：ifCond

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const methodOverride = require('method-override')
 const db = require('./models')
 const app = express()
 const port = process.env.PORT || 3000
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
