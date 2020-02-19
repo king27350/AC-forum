762ea248d58b6b08b59a7c26a3dcf7edd6718845
Chris <shian0222@gmail.com>
2020-02-19 15:54:59 +0800

    依分類篩選餐廳總表

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,13 +4,23 @@ const Category = db.Category
 
 let restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['categoryId'] = categoryId
+    }
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50)
       }))
-      return res.render('restaurants', {
-        restaurants: data
+      Category.findAll().then(categories => {
+        return res.render('restaurants', JSON.parse(JSON.stringify({
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId
+        })))
       })
     })
   },
