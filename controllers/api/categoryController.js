818c4947952181f11a47166d9ef4f54ac5c9d467
Chris <shian0222@gmail.com>
2020-02-26 16:06:43 +0800

    refactor POST /api/admin/restaurants

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -8,7 +8,14 @@ let categoryController = {
     categoryService.getCategories(req, res, (data) => {
       return res.json(data)
     })
-  }
+  },
+
+  postCategories: (req, res) => {
+    categoryService.postCategories(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+
 }
 
 module.exports = categoryController
--- services/categoryServices.js
+++ services/categoryServices.js
@@ -17,7 +17,22 @@ let categoryService = {
         callback({ categories: JSON.parse(JSON.stringify(categories)) })
       }
     })
-  }
+  },
+
+  postCategories: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ status: 'error', message: "name didn\'t exist" })
+    } else {
+      return Category.create({
+        name: req.body.name
+      })
+        .then((category) => {
+          callback({ status: 'success', message: "category was successfully created" })
+        })
+    }
+  },
+
+
 }
 
 module.exports = categoryService
